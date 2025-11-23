679a89b365d9ba80bb7b7d8fea6271ac10b00475
sweetborsh0 <maksimromancenko50@xmail.ru>
2025-11-23 14:45:37 +0300

    Update update_repository.sh

--- update_repository.sh
+++ update_repository.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 set -e
 
-# Пакеты (из input или дефолт)
 PACKAGES="${INPUT_PACKAGES:-fastfetch-git amneziavpn-bin dl-desktop-git}"
 
-echo "Собираю пакеты: $PACKAGES"
+echo "Собираю: $PACKAGES"
 
-# Обновляем систему
+# Обновляем систему (на всякий)
 pacman -Syu --noconfirm
 
-# Билдим каждый
 for pkg in $PACKAGES; do
-  echo "=== $pkg ==="
-  cd /tmp
-  rm -rf "$pkg"
-  git clone https://aur.archlinux.org/"$pkg".git
-  cd "$pkg"
-  makepkg --noconfirm --skippgpcheck  # Без проверки подписей, чтоб не падало
-  cp *.pkg.tar.zst /local_repository/
-  cd ..
+    echo "=== $pkg ==="
+    cd /tmp
+    rm -rf "$pkg"
+    git clone --depth 1 https://aur.archlinux.org/"$pkg".git
+    cd "$pkg"
+
+    # ← ВОТ ЭТО ВСЁ РЕШАЕТ: makepkg сам ставит недостающие зависимости
+    makepkg -s --noconfirm --needed --skippgpcheck || \
+    makepkg -s --noconfirm --needed --skippgpcheck
+
+    cp *.pkg.tar.zst /repo/
 done
 
-# База репо
-cd /local_repository
-repo-add aurci2.db.tar.gz *.pkg.tar.zst
+cd /repo
+repo-add aura.db.tar.gz *.pkg.tar.zst
 
-# Files для pacman
-find . -name "*.pkg.tar.zst" -exec pacman -Qlp {} \; | sort -u > aurci2.files
+echo "package_dir=/repo" >> $GITHUB_OUTPUT
 
-echo "Готово! Файлы в /local_repository:"
-ls -la /local_repository/
-
-# Output для workflow
-echo "package_dir=/local_repository" >> $GITHUB_OUTPUT
+echo "ГОТОВО! Пакеты:"
+ls -lah /repo
