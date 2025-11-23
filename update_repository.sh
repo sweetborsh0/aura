7e7eaf9f941498902c53dc75cb8f99f6e793f5cb
sweetborsh0 <maksimromancenko50@xmail.ru>
2025-11-23 14:56:49 +0300

    Update update_repository.sh

--- update_repository.sh
+++ update_repository.sh
@@ -1,28 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Fail if anything goes wrong.
 set -e
+# Print each line before executing.
+set -x
 
-PACKAGES="${INPUT_PACKAGES:-fastfetch-git amneziavpn-bin dl-desktop-git}"
+# Get list of all packages with dependencies to install.
+packages_with_aur_dependencies="$(aur depends --pkgname $INPUT_PACKAGES $INPUT_MISSING_AUR_DEPENDENCIES)"
+echo "AUR Packages requested to install: $INPUT_PACKAGES"
+echo "AUR Packages to fix missing dependencies: $INPUT_MISSING_AUR_DEPENDENCIES"
+echo "AUR Packages to install (including dependencies): $packages_with_aur_dependencies"
 
-echo "Собираю: $PACKAGES"
+# Sync repositories.
+pacman -Sy
 
-# Обновляем систему
-pacman -Syu --noconfirm
+# Check for optional missing pacman dependencies to install.
+if [ -n "$INPUT_MISSING_PACMAN_DEPENDENCIES" ]
+then
+    echo "Additional Pacman packages to install: $INPUT_MISSING_PACMAN_DEPENDENCIES"
+    pacman --noconfirm -S $INPUT_MISSING_PACMAN_DEPENDENCIES
+fi
 
-for pkg in $PACKAGES; do
-    echo "=== $pkg ==="
-    cd /tmp
-    rm -rf "$pkg"
-    pacman -Sy git
-    git clone --depth 1 https://aur.archlinux.org/"$pkg".git
-    cd "$pkg"
-    makepkg -s --noconfirm --needed --skippgpcheck
-    cp *.pkg.tar.zst /repo/
-done
+# Add the packages to the local repository.
+sudo --user builder \
+    aur sync \
+    --noconfirm --noview \
+    --database aurci2 --root /local_repository \
+    $packages_with_aur_dependencies
 
-cd /repo
-repo-add aura.db.tar.gz *.pkg.tar.zst
-
-echo "package_dir=/repo" >> $GITHUB_OUTPUT
-
-echo "ГОТОВО! Пакеты:"
-ls -lah /repo
+# Move the local repository to the workspace.
+if [ -n "$GITHUB_WORKSPACE" ]
+then
+    rm -f /local_repository/*.old
+    echo "Moving repository to github workspace"
+    mv /local_repository/* $GITHUB_WORKSPACE/
+    # make sure that the .db/.files files are in place
+    # Note: Symlinks fail to upload, so copy those files
+    cd $GITHUB_WORKSPACE
+    rm aurci2.db aurci2.files
+    cp aurci2.db.tar.gz aurci2.db
+    cp aurci2.files.tar.gz aurci2.files
+else
+    echo "No github workspace known (GITHUB_WORKSPACE is unset)."
+fi
